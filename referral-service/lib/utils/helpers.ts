35e923fa6d9bfb845b1427da420b917567b0b83a
Jonathan <jonathan@brave.credit>
2021-11-18 15:59:46 -0800

    removing unnecessary files

--- referral-service/lib/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-export const diffHours = (dt2: Date, dt1: Date) => {
-  let diff = (dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= 60 * 60;
-  return Math.abs(Math.round(diff));
-};
--- referral-service/lib/utils/mailchimp/constants.ts
+++
@@ -1,6 +0,0 @@
-export enum MailchimpTriggerEmails {
-  OnboardingDropOut = 'nt-04-drop-out-of-onboarding',
-  DisputeSubmitted = 'd01-dispute-submitted',
-  DisputeResultsReady = 'd02-dispute-results-ready',
-  ReportRefreshed = 't21-credit-report-refresh',
-}
