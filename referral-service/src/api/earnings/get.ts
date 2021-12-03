aa7ee7f5966776aeb45266676263ae5e97c174f9
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-12-03 12:35:38 -0800

    test payload

--- referral-service/src/api/earnings/get.ts
+++ referral-service/src/api/earnings/get.ts
@@ -1,15 +1,35 @@
-'use strict';
+"use strict";
+import * as interfaces from "lib/interfaces";
+import * as queries from "lib/queries";
+import { response } from "lib/utils/response";
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 
-import { response } from 'lib/utils/response';
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-
-    
-
+export const main: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const id: string = event.requestContext.authorizer?.claims?.sub;
+  const payload: interfaces.IGetReferral = { id };
   try {
-    return response(200, {});
+    return response(200, payload);
   } catch (err) {
     return response(500, err);
   }
 };
+
+// export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//     const id: string = event.requestContext.authorizer?.claims?.sub;
+//     const payload: interfaces.IGetReferral = { id };
+//     const validate = ajv.getSchema<interfaces.IGetReferral>('referralGet');
+//     if (!validate || !validate(payload)) throw `Malformed message=${JSON.stringify(payload)}`;
+//     try {
+//       const { id } = payload;
+//       const referral = await queries.getReferral(id);
+//       return referral ? response(200, referral) : response(404, referral);
+//     } catch (err) {
+//       return response(500, err);
+//     }
+//   };
