149ceb758a5815e4e0b7eb30c38581d7e84b65b5
Jonathan <jonathan@brave.credit>
2021-12-29 17:21:20 -0800

    save

--- analytic-service/src/triggers/scores/triggerScoreTracking.ts
+++ analytic-service/src/triggers/scores/triggerScoreTracking.ts
@@ -21,8 +21,7 @@ export const main: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent): P
           const scoreId = new Date().valueOf();
           const bureauId = newImage.bureauId;
           const score = newImage.currentScore || -1;
-          const source = 'fulfill:unknown'; // could look at the logs and determine
-          const creditScore = new CreditScoreMaker(sub, scoreId, bureauId, score, source);
+          const creditScore = new CreditScoreMaker(sub, scoreId, bureauId, score);
           console.log('credit score ==> ', creditScore);
           await createCreditScore(creditScore);
         }
