dd14c486ae857df872d97367334735a128d6c5f0
NCHitsman <noah.hitsman@gmail.com>
2022-01-05 15:08:21 -0800

    date fix

--- referral-service/lib/data/campaign.ts
+++ referral-service/lib/data/campaign.ts
@@ -1,4 +1,4 @@
 import * as moment from 'moment';
 
 export const CURRENT_CAMPAIGN = 'jan2022';
-export const CURRENT_CAMPAIGN_START_DATE = moment('2022-01-06T00:00:000Z').toISOString();
+export const CURRENT_CAMPAIGN_START_DATE = moment('2022-01-06T00:00:00.000Z').toISOString();
