8aa2fd978107c683e79fe5b908ffda019a1adf8e
Jonathan <jonathan@brave.credit>
2022-01-20 09:00:16 -0800

    save

--- session-service/src/api/update.ts
+++ session-service/src/api/update.ts
@@ -20,6 +20,21 @@ export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):
     } catch (err) {
       return response(500, err);
     }
+  } else if (payload.event === 'disputes_enroll') {
+    try {
+      const session: Partial<Session> = {
+        userId: payload.userId,
+        sessionId: payload.sessionId,
+        sessionExpirationDate: payload.expirationDate,
+      };
+      const updated = await updateSession(session, 3);
+
+      //todo add update to referral
+
+      return updated ? response(200, updated) : response(404, updated);
+    } catch (err) {
+      return response(500, err);
+    }
   } else {
     try {
       const session: Partial<Session> = {
