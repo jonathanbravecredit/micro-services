4b8948c4670e11df7dc0755acd06bbcac7b58013
Jonathan <jonathan@brave.credit>
2022-01-20 19:04:09 -0800

    save

--- referral-service/src/handlers/monitors/referralActiveMonitor.ts
+++ referral-service/src/handlers/monitors/referralActiveMonitor.ts
@@ -1,17 +1,30 @@
 import { SNSEvent, SNSEventRecord, SNSHandler } from 'aws-lambda';
+import { UpdateAppDataInput } from 'lib/aws/api.service';
 import { ISession } from 'lib/interfaces/api/sessions/session.interface';
-import { getCampaign, updateReferralCampaign, updateReferralEligibility } from 'lib/queries';
+import {
+  getCampaign,
+  getReferral,
+  updateReferral,
+  getReferralByCode,
+  updateReferralCampaign,
+  updateReferralEligibility,
+  updateEnrollment,
+} from 'lib/queries';
 import { listUserSessions } from 'lib/queries/sessions/sessions.queries';
+import { AddOnsCalculator } from 'lib/utils/addons/addons';
 
 export const main: SNSHandler = async (event: SNSEvent): Promise<void> => {
-  const records = event.Records.filter((r) => {
+  /*============================================*/
+  //      SESSION UPDATES
+  /*============================================*/
+  const sessions = event.Records.filter((r) => {
     const t1 = r.Sns.Subject === 'sessiondataupdate';
     const { service } = JSON.parse(r.Sns.Message) as { service: string; command: string; message: ISession };
     const t2 = service === 'referralservice';
     return t1 && t2;
   });
 
-  records.forEach((r: SNSEventRecord) => {
+  sessions.forEach((r: SNSEventRecord) => {
     console.log('referral active monitor record: ', JSON.stringify(r));
   });
 
@@ -20,7 +33,7 @@ export const main: SNSHandler = async (event: SNSEvent): Promise<void> => {
   //     and enrolls them in the current campaign.
   try {
     await Promise.all(
-      records.map(async (r) => {
+      sessions.map(async (r) => {
         const { message } = JSON.parse(r.Sns.Message) as { service: string; command: string; message: ISession };
         const sessions = await listUserSessions(message.userId, 20);
         console.log('sessions: ', JSON.stringify(sessions));
@@ -37,6 +50,33 @@ export const main: SNSHandler = async (event: SNSEvent): Promise<void> => {
           const current = await getCampaign(1, 0);
           await updateReferralCampaign(message.userId, current!.campaign);
         }
+      }),
+    );
+  } catch (err) {
+    console.log('error in referral active monitor: ', JSON.stringify(err));
+  }
+
+  /*============================================*/
+  //      APP DATA UPDATES
+  /*============================================*/
+  const appdata = event.Records.filter((r) => {
+    const t1 = r.Sns.Subject === 'transunionenrollment';
+    const { service } = JSON.parse(r.Sns.Message) as { service: string; command: string; message: ISession };
+    const t2 = service === 'referralservice';
+    return t1 && t2;
+  });
+
+  // enrollments only
+  try {
+    await Promise.all(
+      appdata.map(async (r) => {
+        const { message } = JSON.parse(r.Sns.Message) as {
+          service: string;
+          command: string;
+          message: UpdateAppDataInput;
+        };
+        const { id } = message;
+        await updateEnrollment(id);
       }),
     );
   } catch (err) {
