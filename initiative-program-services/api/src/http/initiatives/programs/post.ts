4e49784894299b9fd79eb1729e992c7c050dd877
Jonathan <jonathan@brave.credit>
2022-02-24 11:02:24 -0800

    removing http's for program
    will manually create for now

--- initiative-program-services/api/src/http/initiatives/programs/post.ts
+++ initiative-program-services/api/src/http/initiatives/programs/post.ts
@@ -1,4 +1,9 @@
 import 'reflect-metadata';
-import { SQSEvent, SQSHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import { response } from 'libs/utils/response';
 
-export const main: SQSHandler = async (event: SQSEvent): Promise<void> => {};
+export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // keep wide open for now. Until we have the admin portal up and can guard against with admin IAM group
+
+  return response(200, null);
+};
