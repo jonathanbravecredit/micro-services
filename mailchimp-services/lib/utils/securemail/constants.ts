1a405a6c9b854587def4538eedcaccbafea1f593
Jonathan <jonathan@brave.credit>
2022-03-03 16:49:48 -0800

    removing old reference

--- mailchimp-services/lib/utils/securemail/constants.ts
+++ mailchimp-services/lib/utils/securemail/constants.ts
@@ -1,13 +1,5 @@
-import { SecureMailGenerators } from 'lib/utils/securemail/generators';
-
 export enum SecuremailTriggerEmails {
   PVItems = 'pvItems',
   DisputeSubmitted = 'd01-dispute-submitted',
   DisputeResultsReady = 'd02-dispute-results-ready',
 }
-
-export const SecureMailTriggerGenerators: Record<string, (template: string, content: string) => any> = {
-  [SecuremailTriggerEmails.PVItems]: SecureMailGenerators.generateEmailTemplate,
-  [SecuremailTriggerEmails.DisputeSubmitted]: SecureMailGenerators.generateEmailTemplate,
-  [SecuremailTriggerEmails.DisputeResultsReady]: SecureMailGenerators.generateEmailTemplate,
-};
