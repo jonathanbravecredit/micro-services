e503ed095ce2603475ad8c0e97ce9b4e8a3825f2
Jonathan <jonathan@brave.credit>
2022-03-04 16:46:20 -0800

    adding back 400 messages

--- initiative-program-services/api/src/http/initiatives/get.ts
+++ initiative-program-services/api/src/http/initiatives/get.ts
@@ -11,13 +11,13 @@ export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):
   const programId = '1';
   console.log('sub: ', sub);
   console.log('programId: ', programId);
-  if (!sub) return response(400, null);
+  if (!sub) return response(400, `no id`);
   try {
     const item = await getInitiative(sub, programId);
     console.log('item: ', JSON.stringify(item));
     const program = (await getPrograms(programId)) as Program;
     console.log('program: ', JSON.stringify(program));
-    if (!item || !program) return response(400, null);
+    if (!item || !program) return response(400, `no initiative:${item} or program:${program}`);
     // need to layer in the initiative from client with the context from program
     const initiative = createNewInitiative(item);
     initiative.addProgramTasks(program).filterProgramTasks().enrichWithContext();
