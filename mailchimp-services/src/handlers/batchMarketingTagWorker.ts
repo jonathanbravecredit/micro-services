67340fd22bf6876ccdcda68bbc562173e8a0a7cf
Jonathan <jonathan@brave.credit>
2022-03-16 14:07:54 -0700

    clean up unused imports

--- mailchimp-services/src/handlers/batchMarketingTagWorker.ts
+++ mailchimp-services/src/handlers/batchMarketingTagWorker.ts
@@ -1,21 +1,11 @@
 import 'reflect-metadata';
-import * as _ from 'lodash';
 import { SNS } from 'aws-sdk';
 import { SQSEvent, SQSHandler } from 'aws-lambda';
-import { UpdateAppDataInput } from 'lib/aws/api.service';
-import { parallelScanAppData } from 'lib/queries/appdata.queries';
-import { getLastTwoReports } from 'lib/queries/CreditReport.queries';
-import { getRandomDisputesById } from 'lib/queries/disputes.queries';
-import { IMailchimpPacket, IMarketingData, MailMessage } from 'lib/utils/mailchimp/interfaces';
-import { Mailchimp } from 'lib/utils/mailchimp/mailchimp';
 import { getSecretKey } from 'lib/utils/secrets';
 import { IBatchPayload, IBatchMsg, IAttributeValue } from 'lib/interfaces/batch.interfaces';
-import { getUsersBySub } from 'lib/queries/cognito.queries';
-import { Dispute } from 'lib/models/dispute.model';
-import { CreditReport } from 'lib/models/CreditReport.model';
 import { PubSubUtil } from 'lib/utils/pubsub/pubsub';
 import { BatchTagWorker } from 'lib/utils/batchworker/batchworker';
-// import * as NodeMailMessage from 'nodemailer/lib/mailer/mail-message';
+
 const mailchimpMarketingSKLoc = process.env.MAILCHIMP_MRKT_SECRET_LOCATION || '';
 const POOL = process.env.POOL || '';
 const sns = new SNS({ region: 'us-east-2' });
