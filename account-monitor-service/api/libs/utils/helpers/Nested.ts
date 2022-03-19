16ceccea321f2b4a636a43cdca1e3d8ac1c14503
Jonathan <jonathan@brave.credit>
2022-03-19 13:49:56 -0700

    fixing webpack issue

--- account-monitor-service/api/libs/utils/helpers/Nested.ts
+++ account-monitor-service/api/libs/utils/helpers/Nested.ts
@@ -6,7 +6,7 @@ export class Nested {
    * @param {string} k the key you want to search for
    */
   static find<T>(o: any, k: string): T {
-    let value;
+    let value: any;
     const _returnNestedObject = (obj: any) => {
       Object.keys(obj).forEach((key) => {
         if (key === k && value === undefined) value = obj[k];
@@ -26,7 +26,7 @@ export class Nested {
    * @param {string} k the key you want to search for
    */
   static findAll<T>(o: any, k: string): T[] {
-    let value = [];
+    let value: any[] = [];
     const _returnNestedObject = (obj: any) => {
       Object.keys(obj).forEach((key) => {
         if (key === k) value.push(obj[k]);
