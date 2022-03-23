5d2f51f43a644df5e19f403f8523afc1bdad56ce
Jonathan <jonathan@brave.credit>
2022-03-22 20:56:33 -0700

    adding a counter

--- mailchimp-services/src/handlers/batchInitiativeTags.ts
+++ mailchimp-services/src/handlers/batchInitiativeTags.ts
@@ -6,17 +6,20 @@ export const main: Handler<any, any> = async (event: ScheduledEvent): Promise<vo
   const sns = new SNS();
   const pool = process.env.POOL || '';
   // query the initiative table for everything
+  let counter = 0;
   try {
     const initiatives = await listInitiatives();
     await Promise.all(
       initiatives.map(async (i) => {
         try {
           await updateInitiativeSlightly(i);
+          counter++;
         } catch (err) {
           console.error(err);
         }
       }),
     );
+    console.log(`updated: ${counter} records`);
   } catch (err) {
     console.error(err);
   }
