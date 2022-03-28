cec64ef9325558b550b8a8527b99eee876684bf2
Jonathan <jonathan@brave.credit>
2022-03-28 11:19:30 -0700

    removing unused handler

--- referral-service/src/handlers/monitors/referralActiveMonitor.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-import { SNSEvent, SNSHandler } from 'aws-lambda';
-import { ISession } from 'libs/interfaces/api/sessions/session.interface';
-import { ReferralActivationManager } from 'libs/utils/managers/referralActivationManager';
-
-export const main: SNSHandler = async (event: SNSEvent): Promise<void> => {};
