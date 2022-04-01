b90ee51ad158c06b47c9c91aed848ee46d1df861
Jonathan <jonathan@brave.credit>
2022-04-01 09:25:50 -0700

    wip

--- credit-report-services/api/src/api/creditreports/get.ts
+++ credit-report-services/api/src/api/creditreports/get.ts
@@ -1,15 +1,16 @@
 import 'reflect-metadata';
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import { response } from 'libs/utils/response';
-import { getReport } from 'libs/queries/CreditReport.queries';
-import { MergeReport } from 'libs/models/MergeReport/MergeReport';
+
+// import { getReport } from 'libs/queries/CreditReport.queries';
+// import { MergeReport } from 'libs/models/MergeReport/MergeReport';
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('event ==> ', JSON.stringify(event));
   const sub = event?.requestContext?.authorizer?.claims?.sub;
   if (!sub) return response(200, 'no id provided');
   try {
-    const report = await getReport(sub, 0);
+    const report = await CreditReportQueries; //getReport(sub, 0);
     const resp = {
       ...report,
       report: new MergeReport(report.report),
