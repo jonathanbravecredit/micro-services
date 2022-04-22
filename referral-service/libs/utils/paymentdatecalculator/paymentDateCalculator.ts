b3f3431e2ae762c1e993e890e358b1d88f66d770
Jonathan <jonathan@brave.credit>
2022-04-22 10:19:37 -0700

    payment date calc
    updated the arg to 'maxHit'
    updated comments to be more clear on the logic

--- referral-service/libs/utils/paymentdatecalculator/paymentDateCalculator.ts
+++ referral-service/libs/utils/paymentdatecalculator/paymentDateCalculator.ts
@@ -11,22 +11,22 @@ export class PaymentDateCalculator {
 
   /**
    * Logic:
-   * 1. if in the bonus then next payOn day (ex: tuesday (gladly))
-   * 2. if NOT in the bonus then first payOn of next month
+   * 1. if they hit the max referrals amount then next payOn day (ex: tuesday (gladly))
+   * 2. if NOT in the max then first payOn of next month
    *   - move to start of month (avoids 30/31 and 28 misses)
    *   - add one month to date
    *   - add 6 - payOn to date
    *      - 6 - payOn will move it to sunday (0) or later if curr day is after payOn dat and push the week over
    *      - or, wll keep in the same week if on Sun, Mon, or Tues, etc if before payOn which is correct
    *   - set the date to payOn (ex: Tuesday = 2)
-   * @param bonusHit
+   * @param maxHit
    * @param endDate
    * @returns
    */
-  calcPaymentDate(bonusHit: boolean, endDate: string, payOn: number = 2): string {
+  calcPaymentDate(maxHit: boolean, endDate: string, payOn: number = 2): string {
     const now = dayjs(new Date()).tz();
     const campaignEnd = dayjs(endDate).tz();
-    return bonusHit
+    return maxHit
       ? now.hour(0).minute(0).second(0).millisecond(0).add(7, 'days').day(2).toISOString()
       : campaignEnd
           .startOf('month')
