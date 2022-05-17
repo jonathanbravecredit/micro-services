e750e21ad6ffb2db56f831d479f542d77b01792c
Jonathan <jonathan@brave.credit>
2022-05-16 17:10:19 -0700

    building out comparatives handler

--- credit-report-services/api/src/api/comparatives/get.ts
+++ credit-report-services/api/src/api/comparatives/get.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 import { response } from 'libs/utils/response';
 import { CreditReportQueries as db } from '@bravecredit/brave-sdk';
+import { ReportComparatives } from 'libs/models/report-comparatives/report-comparatives';
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('event ==> ', JSON.stringify(event));
@@ -9,14 +10,9 @@ export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent):
   if (!sub) return response(200, 'no id provided');
   try {
     const [current, prior] = await db.listLastTwoReports(sub);
-
-    // if (!report) return response(200, null);
-    // const resp = {
-    //   ...report,
-    //   report: new MergeReport(report.report),
-    //   metrics: new CreditReportMetrics(report.report), // will need to batch update with metrics
-    // };
-    return response(200, {});
+    const instance = new ReportComparatives(prior, current);
+    instance.run();
+    return response(200, instance.comparatives);
   } catch (err) {
     return response(500, err);
   }
