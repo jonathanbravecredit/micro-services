abae3e3e9f8b45f9e5d33527e9903961deafada0
Jonathan <jonathan@brave.credit>
2022-05-17 08:48:39 -0700

    adding back class logic

--- credit-report-services/api/libs/models/report-comparisons/report-comparisons.ts
+++ credit-report-services/api/libs/models/report-comparisons/report-comparisons.ts
@@ -8,52 +8,54 @@ export class ReportComparisons {
   currMetrics: CreditReportMetric<unknown, unknown>[] = [];
   comparison: Comparisons = {} as Comparisons;
   metrics: string[] = ['negative_accounts', 'credit_mix', 'credit_utilization'];
-  // public prior: CreditReport, public current: CreditReport
-  constructor() {
+  constructor(public prior: CreditReport, public current: CreditReport) {
     console.log('here 0');
     console.log('metrics array', this.metrics);
-    // _.bindAll(this, 'compare');
-    // this.priorMetrics = this.prior.metrics || [];
-    // this.currMetrics = this.current.metrics || [];
-    // console.log('current metricsss', this.currMetrics);
-    // console.log('prior metricsss', this.priorMetrics);
+    _.bindAll(this, 'compare');
+    this.priorMetrics = this.prior.metrics || [];
+    this.currMetrics = this.current.metrics || [];
+    console.log('current metricsss', this.currMetrics);
+    console.log('prior metricsss', this.priorMetrics);
   }
 
-  // run(): void {
-  //   console.log('in run');
-  //   this.check();
-  //   console.log('current metricsss', this.currMetrics);
-  //   console.log('prior metricsss', this.priorMetrics);
-  //   console.log('this.metrics', this.metrics);
-  //   this.metrics.forEach(this.compare);
-  // }
+  run(): void {
+    console.log('in run');
+    this.check();
+    console.log('current metricsss', this.currMetrics);
+    console.log('prior metricsss', this.priorMetrics);
+    console.log('this.metrics', this.metrics);
+    this.metrics.forEach(this.compare);
+  }
 
-  // compare(metric: MetricIds): void {
-  //   const prior = this.priorMetrics.find((m) => m.metricId === metric);
-  //   const current = this.currMetrics.find((m) => m.metricId === metric);
-  //   console.log('current metric', current);
-  //   console.log('prior metric', prior);
-  //   const delta = prior?.metricValue != current?.metricValue ? ComparisonUpdates.Changed : ComparisonUpdates.NoChange;
-  //   this.comparison[metric] = {
-  //     priorValue: prior?.metricValue || null,
-  //     currentValue: current?.metricValue || null,
-  //     delta,
-  //   };
-  //   console.log('this.comparison', this.comparison);
-  // }
+  compare(metric: string): void {
+    const prior = this.priorMetrics.find((m) => m.metricId === metric);
+    const current = this.currMetrics.find((m) => m.metricId === metric);
+    console.log('current metric', current);
+    console.log('prior metric', prior);
+    const delta = prior?.metricValue != current?.metricValue ? ComparisonUpdates.Changed : ComparisonUpdates.NoChange;
+    const data = {
+      [metric]: {
+        priorValue: prior?.metricValue || null,
+        currentValue: current?.metricValue || null,
+        delta,
+      },
+    };
+    this.comparison = _.merge(this.comparison, data);
+    console.log('this.comparison', this.comparison);
+  }
 
-  // check(): void {
-  //   if (this.priorMetrics.length === 0) {
-  //     console.log('here 1');
-  //     const prior = new CreditReportMetrics(this.prior.report).calculateMetrics();
-  //     console.log('prior metrics', prior);
-  //     this.priorMetrics = prior;
-  //   }
-  //   if (this.currMetrics.length === 0) {
-  //     console.log('here 2');
-  //     const current = new CreditReportMetrics(this.current.report).calculateMetrics();
-  //     console.log('current metrics', current);
-  //     this.currMetrics = current;
-  //   }
-  // }
+  check(): void {
+    if (this.priorMetrics.length === 0) {
+      console.log('here 1');
+      const prior = new CreditReportMetrics(this.prior.report).calculateMetrics();
+      console.log('prior metrics', prior);
+      this.priorMetrics = prior;
+    }
+    if (this.currMetrics.length === 0) {
+      console.log('here 2');
+      const current = new CreditReportMetrics(this.current.report).calculateMetrics();
+      console.log('current metrics', current);
+      this.currMetrics = current;
+    }
+  }
 }
