48c605f66ebf454f9ace4559cefdb652751d3ffc
Jonathan <jonathan@brave.credit>
2022-05-17 13:20:44 -0700

    adding back unit tests

--- credit-report-services/api/tests/libs/models/report-comparison.spec.ts
+++ credit-report-services/api/tests/libs/models/report-comparison.spec.ts
@@ -5,43 +5,43 @@ import { CREDIT_REPORT_CURRENT, CREDIT_REPORT_PRIOR_CHANGE } from 'tests/__mocks
 
 const mockMetrics = [MetricIds.NegativeAccounts, MetricIds.CreditMix, MetricIds.CreditUtilization];
 describe('ReportComparison Class', () => {
-  // let instance: ReportComparisons;
-  // let h: any;
-  // beforeEach(() => {
-  //   instance = new ReportComparisons(CREDIT_REPORT_PRIOR_CHANGE as CreditReport, CREDIT_REPORT_CURRENT as CreditReport);
-  //   h = new Helper<ReportComparisons>(instance);
-  // });
+  let instance: ReportComparisons;
+  let h: any;
+  beforeEach(() => {
+    instance = new ReportComparisons(CREDIT_REPORT_PRIOR_CHANGE as CreditReport, CREDIT_REPORT_CURRENT as CreditReport);
+    h = new Helper<ReportComparisons>(instance);
+  });
 
   it('should run one test', () => {
     expect(true).toBeTruthy();
   });
-  // it('should have a method called "compare"', () => {
-  //   expect(h.hasMethod(instance, 'compare')).toEqual(true);
-  // });
+  it('should have a method called "compare"', () => {
+    expect(h.hasMethod(instance, 'compare')).toEqual(true);
+  });
 
-  // it('should have a property called "comparisons"', () => {
-  //   expect(h.hasProperty(instance, 'comparisons')).toEqual(true);
-  // });
-  // it('should have a property called "priorMetrics"', () => {
-  //   expect(h.hasProperty(instance, 'priorMetrics')).toEqual(true);
-  // });
-  // it('should have a property called "currMetrics"', () => {
-  //   expect(h.hasProperty(instance, 'currMetrics')).toEqual(true);
-  // });
-  // it('should have a property called "metrics"', () => {
-  //   expect(h.hasProperty(instance, 'metrics')).toEqual(true);
-  // });
+  it('should have a property called "comparisons"', () => {
+    expect(h.hasProperty(instance, 'comparisons')).toEqual(true);
+  });
+  it('should have a property called "priorMetrics"', () => {
+    expect(h.hasProperty(instance, 'priorMetrics')).toEqual(true);
+  });
+  it('should have a property called "currMetrics"', () => {
+    expect(h.hasProperty(instance, 'currMetrics')).toEqual(true);
+  });
+  it('should have a property called "metrics"', () => {
+    expect(h.hasProperty(instance, 'metrics')).toEqual(true);
+  });
 
-  // describe('run method', () => {
-  //   it('should call check', () => {
-  //     const spy = jest.spyOn(instance, 'check');
-  //     instance.run();
-  //     expect(spy).toHaveBeenCalled();
-  //   });
-  //   it('should call compare x times', () => {
-  //     const spy = jest.spyOn(instance, 'compare');
-  //     instance.run();
-  //     expect(spy).toHaveBeenCalledTimes(3);
-  //   });
-  // });
+  describe('run method', () => {
+    it('should call check', () => {
+      const spy = jest.spyOn(instance, 'check');
+      instance.run();
+      expect(spy).toHaveBeenCalled();
+    });
+    it('should call compare x times', () => {
+      const spy = jest.spyOn(instance, 'compare');
+      instance.run();
+      expect(spy).toHaveBeenCalledTimes(3);
+    });
+  });
 });
