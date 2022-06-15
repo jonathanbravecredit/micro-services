3173a67af92922dc235ae24254347414007552e5
Jonathan <jonathan@brave.credit>
2022-06-15 12:42:37 -0700

    waitlist service updates
    modified the get account by email
     - now uses the path parameter
    added the post account endpoint

---
+++ waitlist-service/api/libs/interfaces/postAccount.interface.ts
@@ -0,0 +1,6 @@
+export interface PostAccount {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
---
+++ waitlist-service/api/src/account/postAccount.ts
@@ -0,0 +1,36 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { PostAccount } from "libs/interfaces/postAccount.interface";
+import { listUsersByEmail } from "libs/queries/cognito";
+import { WaitlistQueries } from "libs/queries/waitlist.queries";
+import { response } from "libs/utils/response";
+import { safeParse } from "libs/utils/safeJson";
+
+const USER_POOL_ID = process.env.POOL || "";
+
+export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const { firstName, lastName, email, phone } = safeParse(event, "body") as PostAccount;
+  if (!firstName) return response(400, `Malformed body; missing firstName: ${firstName}`);
+  if (!lastName) return response(400, `Malformed body; missing lastName: ${lastName}`);
+  if (!email) return response(400, `Malformed body; missing email: ${email}`);
+  if (!phone) return response(400, `Malformed body; missing phone: ${phone}`);
+  if (!USER_POOL_ID) response(400, `Malformed request user pool id: ${USER_POOL_ID}`);
+  try {
+    // get the id from cognito
+    const { Users: users } = await listUsersByEmail(USER_POOL_ID, email);
+    const user = users?.pop();
+    if (!user) return response(400, `Request reject; could not locate user account`);
+    const { Username: id } = user;
+    if (!id) return response(400, `Request reject; could not locate user account`);
+    const account = await WaitlistQueries.createWaitlist({
+      id,
+      firstName,
+      lastName,
+      email,
+      phone,
+    });
+    return response(200, account);
+  } catch (err) {
+    console.error(err);
+    return response(500, err);
+  }
+};
