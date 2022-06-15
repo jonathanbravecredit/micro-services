f4148d9a3f93460517d43a2fb522f906ba1076a2
Jonathan <jonathan@brave.credit>
2022-06-15 10:56:18 -0700

    temporarily removing waitlist table
    need to remove it so I can redefine it with the GSI

--- waitlist-service/api/src/account/getAccount.ts
+++ waitlist-service/api/src/account/getAccount.ts
@@ -2,5 +2,8 @@ import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } f
 import { response } from "libs/utils/response";
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // parse the object from the body
+  // validate the schema from it
+  // get the account using the id
   return response(200, null);
 };
