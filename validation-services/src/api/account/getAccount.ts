ada6eb608b839e217c614f0a373af2841bc528d9
Jonathan <jonathan@brave.credit>
2022-06-15 10:24:03 -0700

    adding getAccounts for validation

--- validation-services/src/api/account/getAccount.ts
+++ validation-services/src/api/account/getAccount.ts
@@ -6,9 +6,9 @@ import { safeParse } from "lib/utils/safeJson";
 const USER_POOL_ID = process.env.POOL || "";
 
 export const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // need to query the cognito system to see if an account with that email already exists
   const { email } = safeParse(event, "pathParameters");
   if (!email) response(400, `Malformed request path parameter: ${email}`);
+  if (!USER_POOL_ID) response(400, `Malformed request user pool id: ${USER_POOL_ID}`);
   try {
     const { Users: users } = await listUsersByEmail(USER_POOL_ID, email);
     return users.length ? response(200, "account_exists") : response(200, "account_not_found");
