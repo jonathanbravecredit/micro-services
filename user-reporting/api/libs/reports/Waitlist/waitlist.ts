fb950e691b25726ff638d3aef09598bd146f22da
Jonathan <jonathan@brave.credit>
2022-06-17 14:55:55 -0700

    waitlist report creation
    added waitlist report to ReportNames enum
    added a waitlist table parallel scan using the generic pScan
    created a WaitlistReport object
    upgraded the sdk to v 1.0.23 which has the waitlist model.
    commented out a bunch of reports that are manually ran and we don't need (just incurring cost and slowing down deployments)
    added the Waitlist to the opsBatchWorker

---
+++ user-reporting/api/libs/reports/Waitlist/waitlist.ts
@@ -0,0 +1,68 @@
+import { OpsReportMaker } from "@bravecredit/brave-sdk/dist/models/ops-report/ops-reports";
+import { Waitlist } from "@bravecredit/brave-sdk/dist/models/waitlist/waitlist";
+import { IBatchMsg, IBatchPayload } from "@bravecredit/brave-sdk/dist/types/batch";
+import { OpsReportQueries } from "@bravecredit/brave-sdk/dist/utils/dynamodb/queries/ops-report.queries";
+import dayjs from "dayjs";
+import { ReportNames } from "libs/data/reports";
+import { parallelScanWaitlist } from "libs/db/waitlist";
+import { IAttributeValue } from "libs/interfaces/batch.interfaces";
+import { ReportBase } from "libs/reports/ReportBase";
+
+export class WaitlistReport extends ReportBase<IBatchMsg<IAttributeValue> | undefined> {
+  constructor(records: IBatchPayload<IBatchMsg<IAttributeValue>>[]) {
+    super(records);
+  }
+
+  async processQuery(
+    esk: IAttributeValue | undefined,
+    segment: number,
+    totalSegments: number,
+  ): Promise<IBatchMsg<IAttributeValue> | undefined> {
+    return await parallelScanWaitlist(esk, segment, totalSegments);
+  }
+
+  async processScan(): Promise<void> {
+    await Promise.all(
+      this.scan?.items.map(async (item: Waitlist) => {
+        const batchId = dayjs(new Date()).add(-5, "hours").format("YYYY-MM-DD");
+        const schema = {};
+        const { id, firstName, lastName, phone, email, referralCode, referredByCode } = item;
+        const ops = new OpsReportMaker(
+          ReportNames.WaitlistAnalytics,
+          batchId,
+          JSON.stringify(schema),
+          JSON.stringify({
+            id,
+            firstName,
+            lastName,
+            phone,
+            email,
+            referralCode,
+            referredByCode,
+          }),
+        );
+        await OpsReportQueries.createOpReport(ops);
+        this.counter++;
+        return true;
+      }),
+    );
+  }
+
+  async processNext() {
+    const scan = this.scan;
+    if (scan?.lastEvaluatedKey != undefined) {
+      const packet: IBatchMsg<IAttributeValue> = {
+        exclusiveStartKey: scan.lastEvaluatedKey,
+        segment: scan.segment,
+        totalSegments: scan.totalSegments,
+      };
+      const payload = this.pubsub.createSNSPayload<IBatchMsg<IAttributeValue>>(
+        "opsbatch",
+        packet,
+        ReportNames.WaitlistAnalytics,
+      );
+      const res = await this.sns.publish(payload).promise();
+      console.log("sns resp ==> ", res);
+    }
+  }
+}
